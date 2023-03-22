c6e48e6439c7741bc4683280fc84547204ef9da6
Nico Rithner <nicorithner@gmail.com>
2023-03-21 20:24:22 -0600

    Create colors constants

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const PRIMARY = {
+  darkCyan: "hsl(158, 36%, 37%)",
+  cream: "hsl(30, 38%, 92%)",
+};
+
+export const NEUTRAL = {
+  veryDarkBlue: "hsl(212, 21%, 14%)",
+  darkGrayishBlue: "hsl(228, 12%, 48%)",
+  white: "hsl(0, 0%, 100%)",
+};

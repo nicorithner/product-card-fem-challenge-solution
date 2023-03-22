1cedf810d5719b53b980c6688efd8647fa1254b9
Nico Rithner <nicorithner@gmail.com>
2023-03-21 20:24:12 -0600

    Add App.tsx style

---
+++ src/App.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { NEUTRAL, PRIMARY } from "./constants";
+
+export const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: ${PRIMARY.cream};
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import ProductCard from "./components/productCard/ProductCard";
 import GlobalStyles from "./GlobalStyles";
+import { AppWrapper } from "./App.styled";
 
 function App() {
   return (
-    <>
+    <AppWrapper>
       <ProductCard />
       <GlobalStyles />
-    </>
+    </AppWrapper>
   );
 }
 
